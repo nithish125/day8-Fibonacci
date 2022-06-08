cf44087df652d1511c671bd117405d8257b92836
nithish125 <nithishs3620@gmail.com>
2022-06-08 16:42:38 +0530

    add

---
+++ fibonacci.js
@@ -0,0 +1,14 @@
+let a =7;
+let num =0;
+let num1=0;
+let num2=1;
+num=num1+num2;
+console.log(num1)
+console.log(num2)
+for(i=0;i<a;i++)
+{
+console.log(+num);
+num1=num2;
+num2=num;
+num=num1+num2;  
+}
